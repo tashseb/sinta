abde8f0ad4fd4ce36b36984e89b73e23883635a2
Tash Cruz <sebastiantashika@gmail.com>
2022-11-22 14:32:17 +0900

    Added sample seeds for the models. Added a photo column to User model

--- db/schema.rb
+++ db/schema.rb
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_081932) do
     t.string "last_name"
     t.string "position"
     t.string "department"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
