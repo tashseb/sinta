13cff17c6ed149bcc5fdcddd544caeb763dc42df
Tash Cruz <sebastiantashika@gmail.com>
2022-11-29 13:45:12 +0900

    added a button to create a candidate

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,9 +4,6 @@
 
 import { application } from "./application"
 
-import AccordionController from "./accordion_controller"
-application.register("accordion", AccordionController)
-
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
