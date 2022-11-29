b66ad1001efc4beb36699364a3ff3d7f2c9cfb21
mohamed shegow <shegowmo@gmail.com>
2022-11-29 15:49:36 +0900

    slack is added

--- app/controllers/api/v1/slack_controller.rb
+++ app/controllers/api/v1/slack_controller.rb
@@ -1,20 +1,28 @@
+require 'json'
+
 class Api::V1::SlackController < ActionController::API
 
   def webhook
     # params coming from Slack
-    payload = JSON.parse(params['payload'])
+    puts params['payload']
+    payload = JSON.parse( params['payload'])
     puts "Payload from Slack:"
-    p payload
+    pp payload
     puts
     # you specified the block_id in the message you built
-    action = payload['message']['blocks'].first['block_id']
-
-    case action
-    when 'whatever action you specified in your block_id'
-      #do stuff
+    interview_id = payload['message']['blocks'][-1]['elements'][0]['alt_text'].to_i
+    @interview = Interview.find(interview_id)
+    sum = payload['state']['values'].values[0..-2].sum do |value|
+      value['static_select-action']['selected_option'] ? JSON.parse(value['static_select-action']['selected_option']['value'])['answer_value'] : 0
     end
-
+    average = sum.to_f/ payload['state']['values'].values[0..-2].length
+    @interview.rating = average
+    @interview.feedback = payload['state']['values'].values[-1]['plain_text_input-action']['value']
+    @interview.save
+    render status: 200, json: { response_type: "ephemeral", text: "Thanks for your feedback!" }.to_json
     head :no_content
   end
+
+
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.hosts << /.+\.jp\.ngrok\.io/
+  config.hosts << /.+\.ap\.ngrok\.io/
 
 
   # Enable server timing
