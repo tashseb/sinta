c34224a33c87fec7f97d1c7ad0a8b26aef0fe3f8
Tash Cruz <sebastiantashika@gmail.com>
2022-12-01 11:56:53 +0900

    updated the notification

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [ :home ]
+  before_action :authenticate_user!, only: [:home]
 
   def home
+    @role = Role.where(user: current_user)
+    @stages = Stage.where(role: @role)
+  end
+
+  def total_interviews
+
   end
 end
