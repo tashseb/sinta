c426545dfff523cc77b7cf6e9293b26c090db4b3
mohamed shegow <shegowmo@gmail.com>
2022-12-01 23:05:29 +0900

    grand finalie added

--- app/javascript/controllers/confetti_controller.js
+++ app/javascript/controllers/confetti_controller.js
@@ -1,12 +1,58 @@
 import { Controller } from "@hotwired/stimulus"
+import confetti from 'canvas-confetti';
 import JSConfetti from 'js-confetti'
+
+
+
+
+console.log("hi")
 
 // Connects to data-controller="confetti"
 export default class extends Controller {
 
   connect() {
-    const jsConfetti = new JSConfetti()
-    jsConfetti.addConfetti()
+    console.log('hi fporm connect');
+
   }
 
-}
+  boom() {
+    const jsConfetti = new JSConfetti()
+    jsConfetti.addConfetti()
+    document.body.classList.add("fun");
+
+    // if (window.confetti) {
+    //   console.log("🎉");
+
+    //   var count = 600;
+    //   var defaults = {
+    //     origin: { y: 0.7 }
+    //   };
+
+    //   function fire(particleRatio, opts) {
+    //     confetti(Object.assign({}, defaults, opts, {
+    //       particleCount: Math.floor(count * particleRatio)
+    //     }));
+    //   }
+
+    //   fire(0.1, {
+    //     spread: 26,
+    //     startVelocity: 55,
+    //   });
+    //   fire(0.3, {
+    //     spread: 60,
+    //   });
+    //   fire(0.3, {
+    //     spread: 100,
+    //     decay: 0.91,
+    //   });
+    //   fire(0.2, {
+    //     spread: 120,
+    //     startVelocity: 0.5,
+    //     decay: 79.92,
+    //   });
+    //   fire(0.4, {
+    //     spread: 12220,
+    //     startVelocity: 4205,
+    //   });
+    }
+  }
