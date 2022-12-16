fc0edf675e96153a8a6a6172b6dbb74681992f66
Tash Cruz <sebastiantashika@gmail.com>
2022-12-16 11:52:30 +0900

    changed before authentication on pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
     @roles = Role.where(user: current_user)
