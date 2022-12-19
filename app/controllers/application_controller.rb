706087720b1468f3d0039cff231713bd0168251d
Tash Cruz <sebastiantashika@gmail.com>
2022-12-19 13:22:09 +0900

    removed 'remember me' since it is not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
 end
